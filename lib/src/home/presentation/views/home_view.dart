c62397d5798244e147a92fe190facd9308b0a10e
SubNato <61242958+SubNato@users.noreply.github.com>
2025-07-11 17:51:01 -0500

    Home page along with product domain and data added and functional

--- lib/src/home/presentation/views/home_view.dart
+++ lib/src/home/presentation/views/home_view.dart
@@ -1,25 +1,53 @@
-import 'package:compair_hub/core/common/app/riverpod/current_user_provider.dart';
-import 'package:compair_hub/core/extensions/context_extensions.dart';
-import 'package:compair_hub/core/extensions/text_style_extensions.dart';
+import 'package:compair_hub/src/home/presentation/sections/categories_section.dart';
+import 'package:compair_hub/src/home/presentation/sections/home_app_bar.dart';
+import 'package:compair_hub/src/home/presentation/sections/products_section.dart';
+import 'package:compair_hub/src/home/presentation/sections/search_section.dart';
+import 'package:compair_hub/src/home/presentation/widgets/promo_banner.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
 
-class HomeView extends ConsumerWidget {
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   static const path = '/home';
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final user = ref.watch(currentUserProvider);
-
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text(
-            user!.name,
-            style: context.theme.textTheme.bodyLarge?.adaptiveColour(context),
-          ),
+      appBar: const HomeAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            const Gap(40),
+            SearchSection(
+              onTap: () {
+                // TODO(Search): Push to search screen
+              },
+            ),
+            const Gap(20),
+            Expanded(
+              child: ListView(
+                children: [
+                  const PromoBanner(),
+                  const Gap(20),
+                  const CategoriesSection(),
+                  const Gap(20),
+                  ProductsSection.popular(
+                    onViewAll: () {
+                      // TODO(Products): Push to Popular Products View
+                    },
+                  ),
+                  const Gap(20),
+                  ProductsSection.newArrivals(
+                    onViewAll: () {
+                      // TODO(Products): Push to New Arrivals Products View
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
